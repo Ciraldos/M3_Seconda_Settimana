abb57fa6c589428779b992ae30f9272576ea8426
Ciraldos <andreaciraldo2@gmail.com>
2024-05-30 16:20:40 +0200

    .

--- Giorno_4/Esercizio_S10_L4/src/app/components/home/home.component.ts
+++ Giorno_4/Esercizio_S10_L4/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from '../../app.service';
+import { iPhoto } from '../../i-photo';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  photoArr:iPhoto[] = [];
+  constructor(private productSvc:AppService){}
+
+  ngOnInit(){
+    this.productSvc.getPhotos().subscribe(photo => {
+      this.photoArr = photo;
+      console.log(this.photoArr)
+    })
+  }
 
 }
